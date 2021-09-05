3510da99583adc06bd7ad73cc4a9555b1e6cac59
YuliDanixa <ecm7908i@claro.com.co>
2021-09-05 07:40:39 -0500

    Se termina segunda API del detalle del producto

--- src/routes/index.js
+++ src/routes/index.js
@@ -44,37 +44,62 @@ router.get("/", (req, res) => {
 });
 
 router.get("/detailItem/:id", (req, res) => {
-    fetch(
-        "https://api.mercadolibre.com/items/" + req.params.id
-    )
-        .then((response) => response.json())
-        .then((dataJson) => {
-            console.log('datajson', dataJson)
-            let detailItem = {
-                "author": {
-                    "name": "Yuli", "lastname": "Bonilla"
-                },
-                "item": {
-                    "id": dataJson.id,
-                    "title": dataJson.title,
-                    "price": {
-                        "currency": dataJson.currency_id,
-                        "amount": dataJson.price,
-                        "decimals": '0'
+    var promise1 = new Promise((resolve, reject) => {
+        fetch(
+            "https://api.mercadolibre.com/items/" + req.params.id
+        )
+            .then((response) => response.json())
+            .then((dataJson) => {
+                let detailItem = {
+                    "author": {
+                        "name": "Yuli", "lastname": "Bonilla"
                     },
-                    "picture": dataJson.pictures,
-                    "condition": dataJson.condition,
-                    "free_shipping": dataJson.shipping.free_shipping,
-                    "sold_quantity": dataJson.sold_quantity,
-                    "description": "String"
+                    "item": {
+                        "id": dataJson.id,
+                        "title": dataJson.title,
+                        "price": {
+                            "currency": dataJson.currency_id,
+                            "amount": dataJson.price,
+                            "decimals": '0'
+                        },
+                        "picture": dataJson.pictures,
+                        "condition": dataJson.condition,
+                        "free_shipping": dataJson.shipping.free_shipping,
+                        "sold_quantity": dataJson.sold_quantity,
+                        "description": ""
+                    }
                 }
-            }
 
-            res.json({ data: detailItem });
+                resolve({ "error": false, data: detailItem })
+            }).catch((e) => {
+                console.log('error', e);
+                reject({ "error": true })
+            })
+    });
+
+    var promise2 = new Promise((resolve, reject) => {
+        fetch(
+            "https://api.mercadolibre.com/items/" + req.params.id + "/description"
+        )
+            .then((response) => response.json())
+            .then((dataJson) => {
+                let descriptionItem = dataJson.plain_text
+                resolve({ "error": "false", data: descriptionItem })
+            }).catch((e) => {
+                console.log('error', e);
+                reject({ "error": true })
+            })
+    });
+
+    Promise.all([promise1, promise2])
+        .then(results => {
+            let dataFormated = results[0]
+            dataFormated.data.item.description = results[1].data
+            res.json({ data: dataFormated });
         }).catch((e) => {
-            console.log('error', e);
             res.status(500).json({ error: e })
-        })
+        }
+        );
 });
 
 module.exports = router;
